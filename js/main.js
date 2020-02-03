1a9115fb02738b4dc683cb70ae4c352c20fa9224
Denis Nadarevic <32303056+bigboydeku@users.noreply.github.com>
2020-02-03 10:53:30 -0500

    made timer responsive to IE browser
    I also made the end time on it 7 due to weird javascript UTC default settings.

--- js/main.js
+++ js/main.js
@@ -290,30 +290,25 @@
          fixedContentPos: false
      });
 
+function makeTimer() {
+         var endTime = new Date("27 March 2020 19:00").getTime();
 
-     function makeTimer() {
-
-         var endTime = new Date("27 March 2020 18:00:00 GMT-05:00");
-         endTime = (Date.parse(endTime) / 1000);
-
-         var now = new Date();
-         now = (Date.parse(now) / 1000);
-
-         var timeLeft = endTime - now;
-
-         var days = Math.floor(timeLeft / 86400);
-         var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-         var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
-         var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+         let now = new Date().getTime();
+         let t = endTime - now;
+         let days = Math.floor(t / (1000 * 60 * 60 * 24));
+         let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+         let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+         let secs = Math.floor((t % (1000 * 60)) / 1000);
 
          if (hours < "10") { hours = "0" + hours; }
          if (minutes < "10") { minutes = "0" + minutes; }
          if (seconds < "10") { seconds = "0" + seconds; }
 
+
          $("#days").html(days + "<span>Days</span>");
          $("#hours").html(hours + "<span>Hours</span>");
-         $("#minutes").html(minutes + "<span>Minutes</span>");
-         $("#seconds").html(seconds + "<span>Seconds</span>");
+         $("#minutes").html(mins + "<span>Minutes</span>");
+         $("#seconds").html(secs + "<span>Seconds</span>");
 
      }
 
